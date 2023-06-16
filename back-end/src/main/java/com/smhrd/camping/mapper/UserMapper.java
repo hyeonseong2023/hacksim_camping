821403e0c1cbea8eec94b6023476c38504602def
이현성 <528abcdef@naver.com>
2023-06-16 14:44:52 +0900

    2:44

--- back-end/src/main/java/com/smhrd/camping/mapper/UserMapper.java
+++ back-end/src/main/java/com/smhrd/camping/mapper/UserMapper.java
@@ -12,10 +12,10 @@ public interface UserMapper {
 	// 회원가입
 	@Insert("INSERT INTO TB_USER (USER_EMAIL, USER_PW, USER_NICK) VALUES(#{inputEmail},#{inputPw},#{inputNick})")
 	public User Join(User user);
-//	
+	
 //	// 로그인
 //	@Select("SELECT * FROM TB_USER WHERE USER_EMAIL=#{inputEmail} AND USER_PW = ${inputPw}")
-//	public User login(User u);
+//	public User Login(User user);
 	
 	
 }	
