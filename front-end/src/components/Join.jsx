d925da61d08efe11291c2847b4ffad6b147221fa
이현성 <528abcdef@naver.com>
2023-06-19 16:19:43 +0900

    2023 0619

--- front-end/src/components/Join.jsx
+++ front-end/src/components/Join.jsx
@@ -1,76 +1,76 @@
-// HJ
 import React, { useEffect, useState } from 'react'
 import '../Login.css'
 import axios from 'axios'
+// export const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 export const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 
 const Join = ({ user_email, setUser_Email, user_pw, setUser_PW, user_nick, setUser_Nick }) => {
 
+
+// =========================================이 밑으로 일단 주석처리
     //     //SNS 로그인 - KAKAO 연동
 
-    const JS_KEY = 'dd382d172dfaf2c763c94fe963356a69'
-    const REDIRECT_URL = 'http://localhost:3000/join'
-    const kakaoLink = 'https://kauth.kakao.com/oauth/authorize?client_id=0ae55e825a9fa3a77d17cdcb413fcb71&redirect_uri=http://localhost:3000/join&response_type=code'
+    // const JS_KEY = 'dd382d172dfaf2c763c94fe963356a69'
+    const REDIRECT_URI = "http://localhost:3000/join";
+    const REST_API_KEY = "c7cdf149cf26d90f317204cd9e5a046f";
+    const kakaoLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`  
 
     //카카오 로그인 페이지로 이동
     const kakao_LoginHandler = () => {
         window.location.href = kakaoLink;
 
     }
- // 응답코드 받기
-    // const code = new URL(window.location.href).searchParams.get("code");
-
-   
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //       try {
-    //         const response = await fetch(`http://172.20.10.3:3000/user/signin?code=${code}`, {
-    //           method: "POST",
-    //           headers: {
-    //             "Content-Type": "application/x-www-form-urlencoded;",
-    //           },
-    //         });
-
-    //         // Handle the response here (e.g., parse JSON, update state, etc.)
-    //       } catch (error) {
-    //         console.error("Error occurred during fetch:", error);
-    //       }
-    //     };
-
-    //     fetchData();
-    //   }, [code]);
-
-    //   console.log(code); 
+ // 응답코드 && 토큰
+ 
 
      
     //토큰 받기
-        useEffect(() => {
-            const params = new URL(document.location.toString()).searchParams;
-            const code =params.get("code");
-            const grant_type = "authorization_code";
-            const client_id = "0ae55e825a9fa3a77d17cdcb413fcb71";
-            const REDIRECT_URI = 'http://localhost:3000/join';
-          
-            axios
-              .post(
-                `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-                {},
-                {
-                  headers: {
-                    "Content-Type": "application/json;charset=UTF-8",
-                  },
-                }
-              )
-              .then((res) => {
-                console.log(res);
-              })
-              .catch((error) => {
-                console.error("Error occurred during axios request:", error);
-              });
-          }, []);
+    // useEffect(() => {
+    //     const params = new URL(document.location.toString()).searchParams;
+    //     const code =params.get("code");
+    //     const grant_type = "authorization_code";
+    //     const client_id = "0ae55e825a9fa3a77d17cdcb413fcb71";
+    //     const REDIRECT_URI = 'http://localhost:3000/join';
+      
+    //     axios
+    //       .post(
+    //         `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+    //         {},
+    //         {
+    //           headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    //           },
+    //         }
+    //       )
+    //       .then((res) => {
+    //         console.log(res);
+
+
+    //     //     const{data} = res;
+    //     //     const {access_token} = data;
+    //     //     if(access_token){
+    //     //         console.log(`Bearer ${access_token}`);
+    //     //         axios
+    //     //         .post("https://kapi.kakao.com/v2/user/me",
+    //     //         {},
+    //     //         {
+    //     //             header:{
+    //     //                 Authorization : `Bearer ${access_token}`,
+    //     //                 "Content-Type": "application/x-www-form-urlencoded",
+    //     //             },
+
+    //     //         }
+    //     //         )
+    //     //     }
+    //     //   }).then((res)=>{
+    //     //     console.log('데이터성공 ㅠㅠㅠ');
+    //     //     console.log(res);
+    //     //   })
+    //       });
+    //   }, []);
     // 카카오 끝
 
-
+// ============= 이 위로 일단 주석처리
 
 
 
@@ -79,16 +79,6 @@ const Join = ({ user_email, setUser_Email, user_pw, setUser_PW, user_nick, setUs
     const [result, setResult] = useState('');
 
     // 중복 이메일 체크
-=======
-import React, { useState } from 'react'
-import '../Login.css'
-import axios from 'axios'
-const Join = ({user_email, setUser_Email, user_pw,setUser_PW,user_nick, setUser_Nick }) => {
-
-
-    const [result, setResult] = useState('');
-
-// HSeong
     const handleCheckEmail = (e) => {
         e.preventDefault(); // 기본 동작 방지
         axios
@@ -107,11 +97,8 @@ const Join = ({user_email, setUser_Email, user_pw,setUser_PW,user_nick, setUser_
             });
     };
 
-// HJ
     // 중복 끝
 
-=======
-// HSeong
     const handleUseremailChange = (e) => {
         setUser_Email(e.target.value);
         console.log(e.target.value);
@@ -143,11 +130,7 @@ const Join = ({user_email, setUser_Email, user_pw,setUser_PW,user_nick, setUser_
                 console.log("이메일 = ", requestData.user_email);
                 console.log("PW =  ", requestData.user_pw);
                 console.log("nick =  ", requestData.user_nick);
-// HJ
                 alert(user_email + '님 회원가입을 축하드립니다😉')
-=======
-                alert(user_email+'님 회원가입을 축하드립니다😉')
-// HSeong
             })
             .catch(error => {
                 console.error(error); // 오류 발생 시 에러 로그를 출력
@@ -157,7 +140,6 @@ const Join = ({user_email, setUser_Email, user_pw,setUser_PW,user_nick, setUser_
     };
 
     return (
-// HJ
       
         <div id='login-container'>
            
@@ -180,49 +162,15 @@ const Join = ({user_email, setUser_Email, user_pw,setUser_PW,user_nick, setUser_
 
                             <img id='k' onClick={kakao_LoginHandler} src='https://cdn-icons-png.flaticon.com/512/2111/2111496.png' />
                             <img id='g' src='https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/google-icon.png' />
-=======
-        // <div id='container'>
-        <div id='login-container'>
-            <body>
-                <div class="main">
-                    <h1 class="logo">회원가입</h1>
-                    <div id="login_container">
-                        {/* <div id='emailcontainer'> */}
-                        <input type="text" value={user_email} onChange={handleUseremailChange}  placeholder="Email" id="id" class="account"></input>
-                        
-
-                        {/* 중복 이메일 체크 */}
-                        <button type="button" onClick={handleCheckEmail}>중복 확인</button>
-                            <div>{result}</div>
-
-                        {/* </div> */}
-                        <input type="password" value={user_pw} onChange={handleUserPwChange} placeholder="Password" id="password" class="account" />
-                        <button onClick={handleFormSubmit} id="login" class="account">회원가입</button>
-                        <p id="alert" class="account"> </p>
-                        <hr/>
-                        <div id='logintxt_container'>
-                        <div id ='login_text'>SNS 회원가입</div>
-                        </div>
-                        <div id='img_container'>
-                        <img id='k' src='https://cdn-icons-png.flaticon.com/512/2111/2111496.png' />
-                        <img id='g' src='https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/google-icon.png' />
-// HSeong
                         </div>
                     </div>
                 </div>
                 <script src="script.js"></script>
-// HJ
           
         </div>
 
     )
 
-=======
-            </body>
-        </div>
-        // </div>
-    )
-// HSeong
 }
 
 export default Join
