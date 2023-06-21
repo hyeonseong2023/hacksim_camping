7ffea4fccad065c667309b294bf484c6db16c13c
임혜주 <lakm1225@gmail.com>
2023-06-21 17:25:55 +0900

    이거이거

--- front-end/src/components/Logout.jsx
+++ front-end/src/components/Logout.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
+
     const nav = useNavigate();
+    
     useEffect(()=>{
     localStorage.removeItem("loginSuccess");
     localStorage.removeItem("user_email");
